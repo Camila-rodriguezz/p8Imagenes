3b7573c1cfe70d8feee68e471d298a2dbd0ad669
CAMILA_RODRIGUEZ_RUIZ <a.22308051281300@cbtis128.edu.mx>
2025-02-25 16:58:33 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,71 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Mis imagenes",
+            style: TextStyle(
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: const Color(0xffeddca9),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Camila Rodriguez Ruiz Matricula: 22308051281300',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Camila-rodriguezz/Imagenes-para-APP-flutter-6J/refs/heads/main/estetica1.jpg', // Reemplaza con la URL de tu primera imagen
+                width: 200, // Opcional: ajusta el ancho
+                height: 200, // Opcional: ajusta la altura
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              // Espacio entre la imagen y el subtítulo
+              const Text(
+                'Peluqueria rosa en github',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Camila-rodriguezz/Imagenes-para-APP-flutter-6J/refs/heads/main/estetica2.png', // Reemplaza con la URL de tu segunda imagen
+                width: 200, // Opcional: ajusta el ancho
+                height: 200, // Opcional: ajusta la altura
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              // Espacio entre la imagen y el subtítulo
+              const Text(
+                'Peluqueria 2 en github',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
+    ); // material APP
+  } // widgets
+} // clase MiImagen
